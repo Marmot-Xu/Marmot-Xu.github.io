682b98abc539b08752c937478325c352433e2f8b
unknown <xuxuhui2007@163.com>
2021-04-05 03:24:37 +0800

    photos

--- js/init.js
+++ js/init.js
@@ -515,7 +515,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["斜杠青年", "编程爱好者", "区块链爱好者"],
+			strings: ["Slash youth", "Programming enthusiast", "Blockchain enthusiast"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
